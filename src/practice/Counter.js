2c09234716754e4c72799bb1ca796bc5799da8e5
Ivan Lim <uxprogrammer@gmail.com>
2022-05-09 09:34:53 +0800

    allow to change the increase value

--- src/practice/Counter.js
+++ src/practice/Counter.js
@@ -2,11 +2,15 @@ import { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(1);
 
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <input type="number" onChange={(e) => setIncrement(e.target.value)} />
+      <button onClick={() => setCount(count + Number.parseInt(increment))}>
+        Click me
+      </button>
     </div>
   );
 };
